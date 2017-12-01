dfc22979d14dbe2351660ac85eb38ba05cd7a082
Sanjeev Sekar <sankumsek@gmail.com>
2017-11-30 16:46:12 -0800

    Initial commit

---
+++ WebApplication2/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ASPNETCoreIdentity.Models;
+
+namespace ASPNETCoreIdentity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
